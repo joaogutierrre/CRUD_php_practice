61d3559e9284139ff283d390b19a617d6bc2b5ab
João Gutierre <gutierrejoao@gmail.com>
2020-04-02 09:13:48 -0300

    Criando página de login

---
+++ menu.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Menu</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://kit.fontawesome.com/8038ba999d.js" crossorigin="anonymous"></script>
+</head>
+<body>
+	<div class="container" style="padding-top: 10em;"> 
+		<div class="card border-primary">
+			<div class="card-header">
+				<h3 style="position: relative;" class="text-center dark">Gerenciamento</h3>
+			</div>
+				<div class="card-body">
+					<table class="table table-borderless">
+						<thead class="text-center thead-dark">
+							<tr>
+								<th>Cadastros</th>
+								<th>Listagens</th>
+							</tr>
+						</thead>
+						<tbody class="text-center">
+							<tr>
+								<td>
+									
+								  		<div class="col-sm-12">
+								    		<div class="card border-none">
+								      			<div class="card-body">
+								        			<h5 class="card-title text-primary">Cadastro de Produtos</h5>
+								        			<p class="card-text">Cadastre um produto no sistema</p>
+								        			<a href="produtos/form_cadastro_produtos.php" class="btn btn-secondary">Ir</a>
+								      			</div>
+								    		</div>
+								  		</div>
+									
+								</td>
+								<td>
+									
+										<div class="col-sm-12">
+											<div class="card">
+												<div class="card-body">
+													<h5 class="card-title text-primary">Listagem de Produtos</h5>
+													<p class="card-text">Liste, e altera produtos do sistema.</p>
+													<a href="produtos/listagem_produtos.php" class="btn btn-secondary">Ir</a>
+												</div>
+											</div>
+										</div>
+									
+								</td>
+							</tr>
+							<tr>
+								<td>	
+									
+										<div class="col-sm-12">
+											<div class="card">
+												<div class="card-body">
+													<h5 class="card-title text-primary">Cadastro de Categorias</h5>
+													<p class="card-text">Cadastre uma categoria.</p>
+													<a href="categorias/form_cadastrar_categoria.php" class="btn btn-secondary">Ir</a>
+												</div>
+											</div>
+										</div>
+									
+								</td>
+								<td>
+									
+										<div class="col-sm-12">
+											<div class="card">
+												<div class="card-body">
+													<h5 class="card-title text-primary">Listagem de Categorias</h5>
+													<p class="card-text">Liste, e altera uma categoria de produto.</p>
+													<a href="categorias/listagem_categoria.php" class="btn btn-secondary">Ir</a>
+												</div>
+											</div>
+										</div>
+									
+								</td>
+							</tr>
+							<tr>
+								<td>
+									
+										<div class="col-sm-12">
+											<div class="card">
+												<div class="card-body">
+													<h5 class="card-title text-primary">Cadastro de Fornecedores</h5>
+													<p class="card-text">Cadastre um fornecedor</p>
+													<a href="fornecedor/form_fornecedor.php" class="btn btn-secondary">Ir</a>
+												</div>
+											</div>
+										</div>
+									
+								</td>
+								<td> 
+									
+										<div class="col-sm-12">
+											<div class="card">
+												<div class="card-body">
+													<h5 class="card-title text-primary">Listagem de Fornecedores</h5>
+													<p class="card-text">Lista, e altera um fornecedor.</p>
+													<a href="fornecedor/listagem_fornecedor.php" class="btn btn-secondary">Ir</a>
+												</div>
+											</div>
+										</div>
+									
+								</td>
+							</tr>
+						</tbody>
+					</table>
+			</div>
+			
+			
+		</div>
+	</div>
+		
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+</body>
+</html>
