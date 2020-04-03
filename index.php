6dc8c31d09172e249e9be97a59eca8350351c3ad
João Gutierre <gutierrejoao@gmail.com>
2020-04-03 06:39:59 -0300

    Tela de login adicionada

--- index.php
+++ index.php
@@ -2,22 +2,29 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 	<script src="https://kit.fontawesome.com/8038ba999d.js" crossorigin="anonymous"></script>
-	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-	<div class="container">
+	<div class="container col-sm-3" id="containerIndex">
+		<center>
+			<img src="https://img.icons8.com/cute-clipart/100/000000/password.png"/>
+		</center>
 		<form>
 			<div class="from-group">
 				<label>Login</label>
-				<input class="form-control" type="text" name="login"></input>
+				<input class="form-control" type="text" name="login" autocomplete="off" required=""></input>
 			</div>
 			<div class="form-group">
 				<label>Senha</label>
-				<input class="form-control" type="text" name="senha"></input>
+				<input class="form-control" type="password" name="senha" autocomplete="off" required=""></input>
+			</div>
+			<div class="form-group" align="right">
+				<button type="submit" class="btn btn-success">Enviar</button>
 			</div>
 		</form>
 	</div>
